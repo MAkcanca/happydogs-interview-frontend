03bcfb8c7062a51521d01e23bde1cdb99627f333
Mustafa Akcanca <MAkcanca@users.noreply.github.com>
2021-06-09 15:20:14 +0300

    Added calendar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import RevoCalendar from 'revo-calendar'
 import './App.css';
 
 function App() {
@@ -20,7 +21,16 @@ function App() {
       <div className="container">
         <div className="row">
           <div className="col">
-            <p>Paragraph</p>
+            <RevoCalendar
+              style={{
+                height: '35em'
+              }}
+              primaryColor="rgba(254,221,17,0.32)"
+              secondaryColor="#fff"
+              todayColor="rgba(255,155,17,0.32)"
+              sidebarDefault={false}
+              allowDeleteEvent={false}
+            />
           </div>
         </div>
       </div>
